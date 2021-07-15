3510e520b0af7f7654af9b3b06ba64ab374a14e3
DKUMAR196 <84921216+DKUMAR196@users.noreply.github.com>
2021-07-15 19:45:02 +0530

    Java Regex Problem

---
+++ Day19_User_Registration_Java_Regex/src/com/bridgelabz/program/AllEmailProvidedSeparatelyUC9.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.program;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class AllEmailProvidedSeparatelyUC9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);				
+		System.out.println("Enter Your All Email Samples Provided Separately");
+		String password = sc.next();
+		System.out.println(Pattern.matches("^[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)$", password));
+	}
+}
